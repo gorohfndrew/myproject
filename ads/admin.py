93a64cb318f2c766606783d3ad3cf0335d1752d0
gorohandrew <bydnikov8480@gmail.com>
2025-02-02 20:59:34 +0200

    добавлено улучшение

--- ads/admin.py
+++ ads/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib import admin
 from .models import Category, Ad
 
 admin.site.register(Category)
--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -9,8 +9,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+# Импортируем приложение Flask
+from mydona.app import app  # Замените 'app' на имя вашего файла Flask-приложения
 
+# Назначаем переменную application для использования в Gunicorn
+application = app
+
+# Если у вас есть Django настройки, то используйте их как ранее
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
+# Для Django приложения также указываем вызов get_wsgi_application (это для Django)
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
