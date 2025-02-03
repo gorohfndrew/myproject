c35a5c2d9055feee426c1ff1d704bfcbf0195406
gorohandrew <bydnikov8480@gmail.com>
2025-02-03 12:40:56 +0200

    Исправлен wsgi.py и обновлена конфигурация

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -9,13 +9,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from mydona.app import app  # Flask-приложение
 
-# Устанавливаем переменную окружения для Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
-# Создаем WSGI приложение для Django
-django_application = get_wsgi_application()
-
-# Совмещаем Django и Flask приложения (это необычно и сложнее)
-application = app  # Для Flask-приложения можно использовать Flask, если это необходимо
+application = get_wsgi_application()
