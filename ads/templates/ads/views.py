ad9420ab588bcf2612d8457499bd29abbe45042d
gorohandrew <bydnikov8480@gmail.com>
2025-02-04 21:04:40 +0200

    Initial commit with project setup

--- ads/templates/ads/views.py
+++ ads/templates/ads/views.py
@@ -5,8 +5,8 @@ from .models import Ad, Category
 from .serializers import AdSerializer, CategorySerializer
 from .forms import AdForm
 from django.contrib.auth.decorators import login_required
-from .forms import AdForm
 
+# Функция для добавления нового объявления
 @login_required
 def add_ad(request):
     """Создание нового объявления"""
@@ -22,70 +22,21 @@ def add_ad(request):
 
     return render(request, 'ads/add_ad.html', {'form': form})
 
-# ✅ ПРАВИЛЬНО: Объявляем функцию ads_list
+# Функция для отображения списка объявлений
 def ads_list(request):
     ads = Ad.objects.all()
     return render(request, 'ads/ads_list.html', {'ads': ads})
 
+# Функция для отображения правил
 def site_rules(request):
     return render(request, 'ads/rules.html')
 
+# Функция для отображения подробной информации об одном объявлении
 def ad_detail(request, ad_id):
     ad = get_object_or_404(Ad, id=ad_id)
     return render(request, 'ads/ad_detail.html', {'ad': ad})
 
-def add_ad(request):
-    if request.method == 'POST':
-        form = AdForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('ads_list')
-    else:
-        form = AdForm()
-    return render(request, 'ads/add_ad.html', {'form': form})
-
-# Добавляем URL-маршруты
-urlpatterns = [
-    path('', ads_list, name='ads_list'),  # Главная страница объявлений
-    path('rules/', site_rules, name='site_rules'),  # Страница правил
-    path('ad/<int:ad_id>/', ad_detail, name='ad_detail'),  # Страница одного объявления
-    path('add/', add_ad, name='add_ad'),  # Форма добавления объявления
-]
-
-# ViewSet для Category
-class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-
-# ViewSet для Ad
-class AdViewSet(viewsets.ModelViewSet):
-    queryset = Ad.objects.all()
-    serializer_class = AdSerializer
-from .forms import AdForm
-
-# ✅ ПРАВИЛЬНО: Объявляем функцию ads_list
-def ads_list(request):
-    ads = Ad.objects.all()
-    return render(request, 'ads/ads_list.html', {'ads': ads})
-
-def site_rules(request):
-    return render(request, 'ads/rules.html')
-
-def ad_detail(request, ad_id):
-    ad = get_object_or_404(Ad, id=ad_id)
-    return render(request, 'ads/ad_detail.html', {'ad': ad})
-
-def add_ad(request):
-    if request.method == 'POST':
-        form = AdForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('ads_list')
-    else:
-        form = AdForm()
-    return render(request, 'ads/add_ad.html', {'form': form})
-
-# Добавляем URL-маршруты
+# URL-маршруты
 urlpatterns = [
     path('', ads_list, name='ads_list'),  # Главная страница объявлений
     path('rules/', site_rules, name='site_rules'),  # Страница правил
