be672a651b24762a35b2ebc49825f7e219d2c63c
gorohandrew <bydnikov8480@gmail.com>
2025-02-04 22:04:13 +0200

    Initial commit with project setup

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from ads.templates.ads.views import ads_list  # <-- Импортируем ads_list правильно
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', ads_list, name='ads_list'),  # <-- Добавляем представление напрямую
-    path('ads/', include('ads.urls')),  # <-- Подключаем маршруты приложения ads
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', ads_list, name='ads_list'),
+    path('ads/', include('ads.urls')),
 ]
+
+
