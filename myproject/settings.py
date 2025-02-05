7e93882e1d14a65d5be5b05925ba41fae783b061
gorohandrew <bydnikov8480@gmail.com>
2025-02-05 10:43:36 +0200

    Initial commit with project setup

--- myproject/settings.py
+++ myproject/settings.py
@@ -5,10 +5,9 @@ from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-bk%dv%#vk9begu*jchd=)o2_(3)(_(3c!m*at2tn_=g1+5al2k'
+SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
 DEBUG = True
-
-ALLOWED_HOSTS = ['myproject-3-r2ko.onrender.com', 'localhost', '127.0.0.1', 'myproject-4wr3.onrender.com']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
