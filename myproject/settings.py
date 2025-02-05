188d4220991bfccc13110ac3893c633218a64d12
gorohandrew <bydnikov8480@gmail.com>
2025-02-05 10:51:23 +0200

    Initial commit with project setup

--- myproject/settings.py
+++ myproject/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
 DEBUG = True
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = ['myproject-3-r2ko.onrender.com', 'localhost', '127.0.0.1', 'myproject-4wr3.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
