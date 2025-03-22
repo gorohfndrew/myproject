c2d2ad94cdbb9767748afbfb978751c5974ed261
gorohandrew <bydnikov8480@gmail.com>
2025-03-22 21:05:08 +0200

    Сообщение коммита

--- ads/signals.py
+++ ads/signals.py
@@ -3,12 +3,16 @@ from django.dispatch import receiver
 from django.conf import settings
 from .models import Profile
 
+# Сигнал для создания профиля при создании пользователя
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
+        # Создание профиля для нового пользователя
         Profile.objects.create(user=instance)
 
+# Сигнал для сохранения профиля при изменении пользователя
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def save_user_profile(sender, instance, **kwargs):
+    # Проверка, если профиль существует, сохраняем его
     if hasattr(instance, 'profile'):
         instance.profile.save()
